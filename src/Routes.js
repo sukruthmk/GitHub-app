0e76b869b1bb802270bf88ef7578365ccf6104fd
Sukruth M K <suk_mallur@yahoo.co.in>
2019-06-02 19:55:54 -0700

    Added routes

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Login from "./components/login/login";
+import Callback from "./components/common/callback/callback";
+
+class Routes extends Component {
+  render() {
+    const { checkingSession, postCallback } = this.props;
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/callback"
+            render={() => {
+              return <Callback postCallback={postCallback} />;
+            }}
+          />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Routes;
