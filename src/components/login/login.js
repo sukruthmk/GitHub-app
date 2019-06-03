4917b857318dc0842deb62aebc1701ad8f57714c
Sukruth M K <suk_mallur@yahoo.co.in>
2019-06-02 20:03:29 -0700

    added login component

---
+++ src/components/login/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Loading from "../common/loading/loading";
+
+class Login extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return <Loading />;
+  }
+}
+
+export default Login;
