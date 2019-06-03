a1c08e7b8919111125183b082574ba85bf0ceec6
Sukruth M K <suk_mallur@yahoo.co.in>
2019-06-02 22:29:56 -0700

    added helper url function

---
+++ src/components/common/url/url.js
@@ -0,0 +1,19 @@
+const getUrlVars = () => {
+  var vars = {};
+  window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
+    m,
+    key,
+    value
+  ) {
+    vars[key] = value;
+  });
+  return vars;
+};
+
+export const getUrlParam = (parameter, defaultvalue) => {
+  var urlparameter = defaultvalue;
+  if (window.location.href.indexOf(parameter) > -1) {
+    urlparameter = getUrlVars()[parameter];
+  }
+  return urlparameter;
+};
