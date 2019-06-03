a528431a93155142b409e60b5457ea5e781d9b92
Sukruth M K <suk_mallur@yahoo.co.in>
2019-06-03 04:17:54 -0700

    added star/unstar feature

--- src/components/starred/starred.js
+++ src/components/starred/starred.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
+import { graphql, compose } from "react-apollo";
+import { gql } from "apollo-boost";
 import { Button } from "react-bootstrap";
 import styled from "styled-components";
 
-import { GET_REPOSITORIES } from "./api";
+import { ADD_STAR, GET_REPOSITORIES, REMOVE_STAR } from "./api";
 import Loading from "../common/loading/loading";
 import RepositoriesList from "../common/repositoriesList/repositoriesList";
 
@@ -20,14 +21,25 @@ const Content = styled.div`
 
 class Starred extends Component {
   render() {
-    const { error, loading, repositories, loadMore } = this.props;
+    const {
+      addStar,
+      error,
+      loading,
+      repositories,
+      loadMore,
+      removeStar
+    } = this.props;
     return (
       <div>
         {error && <div>{error}</div>}
         {loading && <Loading />}
         {!loading && repositories && (
           <React.Fragment>
-            <RepositoriesList repositories={repositories} />
+            <RepositoriesList
+              repositories={repositories}
+              addStar={addStar}
+              removeStar={removeStar}
+            />
             <Container>
               <Content>
                 <Button onClick={loadMore}>Load More</Button>
@@ -39,43 +51,82 @@ class Starred extends Component {
     );
   }
 }
-
-export default graphql(GET_REPOSITORIES, {
-  props: ({ data: { error, loading, viewer, fetchMore } }) => {
-    let starredRepositories = null;
-    if (viewer) {
-      starredRepositories = viewer.starredRepositories;
-    }
-    return {
-      repositories: starredRepositories ? starredRepositories.nodes : null,
-      loading,
-      error,
-      loadMore: () =>
-        fetchMore({
-          variables: { after: starredRepositories.pageInfo.endCursor },
-          updateQuery: (previousResult = {}, { fetchMoreResult = {} }) => {
-            const previousSearch =
-              previousResult.viewer.starredRepositories || {};
-            const currentSearch =
-              fetchMoreResult.viewer.starredRepositories || {};
-            const previousNodes = previousSearch.nodes || [];
-            const currentNodes = currentSearch.nodes || [];
-            // Specify how to merge new results with previous results
-            return {
-              viewer: {
-                ...previousResult,
-                starredRepositories: {
-                  ...previousSearch,
-                  nodes: [...previousNodes, ...currentNodes],
-                  pageInfo: currentSearch.pageInfo
+export default compose(
+  graphql(GET_REPOSITORIES, {
+    props: ({ data: { error, loading, viewer, fetchMore } }) => {
+      let starredRepositories = null;
+      if (viewer) {
+        starredRepositories = viewer.starredRepositories;
+      }
+      return {
+        repositories: starredRepositories ? starredRepositories.nodes : null,
+        loading,
+        error,
+        loadMore: () =>
+          fetchMore({
+            variables: { after: starredRepositories.pageInfo.endCursor },
+            updateQuery: (previousResult = {}, { fetchMoreResult = {} }) => {
+              const previousSearch =
+                previousResult.viewer.starredRepositories || {};
+              const currentSearch =
+                fetchMoreResult.viewer.starredRepositories || {};
+              const previousNodes = previousSearch.nodes || [];
+              const currentNodes = currentSearch.nodes || [];
+              // Specify how to merge new results with previous results
+              return {
+                viewer: {
+                  ...previousResult,
+                  starredRepositories: {
+                    ...previousSearch,
+                    nodes: [...previousNodes, ...currentNodes],
+                    pageInfo: currentSearch.pageInfo
+                  }
                 }
-              }
-            };
+              };
+            }
+          })
+      };
+    },
+    options: {
+      notifyOnNetworkStatusChange: true
+    }
+  }),
+  graphql(ADD_STAR, {
+    props: ({ mutate }) => ({
+      addStar: starrableId =>
+        mutate({
+          variables: { starrableId },
+          update: proxy => {
+            proxy.writeFragment({
+              id: `Repository:${starrableId}`,
+              fragment: gql`
+                fragment repository on Repository {
+                  viewerHasStarred
+                }
+              `,
+              data: { viewerHasStarred: true, __typename: "Repository" }
+            });
           }
         })
-    };
-  },
-  options: {
-    notifyOnNetworkStatusChange: true
-  }
-})(Starred);
+    })
+  }),
+  graphql(REMOVE_STAR, {
+    props: ({ mutate }) => ({
+      removeStar: starrableId =>
+        mutate({
+          variables: { starrableId },
+          update: proxy => {
+            proxy.writeFragment({
+              id: `Repository:${starrableId}`,
+              fragment: gql`
+                fragment repository on Repository {
+                  viewerHasStarred
+                }
+              `,
+              data: { viewerHasStarred: false, __typename: "Repository" }
+            });
+          }
+        })
+    })
+  })
+)(Starred);
